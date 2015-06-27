caef18157f3a8822464f2732d7f98791b9d5db14
Cristobal <cristobal@cristobal.(none)>
2015-06-27 20:00:25 +0100

    Corrección por cambio de version de sequalize"

--- models/models.js
+++ models/models.js
@@ -27,8 +27,8 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; 
 
-sequelize.sync().success(function() {
-  Quiz.count().success(function (count) {
+sequelize.sync().then(function() {
+  Quiz.count().then(function (count) {
     if(count === 0) {  
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma',
